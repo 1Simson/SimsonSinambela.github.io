f103e2e5293d1f82c2b243e86acc97418226d720
1Simson <jhonbellsonyanfeng@gmail.com>
2025-03-18 23:52:12 +0700

    Add files via upload

---
+++ Belajar JavaScript/main.js
@@ -0,0 +1,17 @@
+document.title = 'John'
+const body = document.body
+body.append('hello world')
+
+const h1 = document.createElement('h1')
+h1.textContent ='Mama Milkol mama milkol'
+
+const namaSaya = document.createElement('p')
+namaSaya.innerHTML ='jadi klian ikut minggu apa gimana sayang '
+
+const namakamu = document.createElement('b')
+namakamu.innerText = 'Ketempat bos samsul'
+
+body.append(h1)
+body.append(namaSaya)
+body.append(namakamu)
+
